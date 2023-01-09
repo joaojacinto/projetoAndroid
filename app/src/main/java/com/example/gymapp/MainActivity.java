3af46eafc66829b06ed9ac3a8c4dd8c635c8de62
joaojacinto <joaorsj96@gmail.com>
2023-01-09 13:13:09 +0000

    implementação

--- app/src/main/java/com/example/gymapp/MainActivity.java
+++ app/src/main/java/com/example/gymapp/MainActivity.java
@@ -3,6 +3,19 @@ package com.example.gymapp;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.view.View;
+import android.widget.EditText;
+
+import com.android.volley.Request;
+import com.android.volley.RequestQueue;
+import com.android.volley.Response;
+import com.android.volley.VolleyError;
+import com.android.volley.toolbox.JsonObjectRequest;
+import com.android.volley.toolbox.StringRequest;
+import com.android.volley.toolbox.Volley;
+
+import org.json.JSONException;
+import org.json.JSONObject;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -10,5 +23,21 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+    }
+
+    public void pesquisarUser(View view){
+        String email = ((EditText)findViewById(R.id.edEmail)).getText().toString();
+
+        JSONObject jsonObj = new JSONObject();
+        try {
+            jsonObj.put("users", email);
+        } catch (JSONException e) {
+            e.printStackTrace();
+        }
+
+        RequestQueue queue = Volley.newRequestQueue(getApplicationContext());
+        String url = "http://192.168.1.215:5000/search";
+
+
     }
 }
