c6b80e4e925161fbddd27121ccd8dca40ff415c2
RGKXD <filipe.gat8@gmail.com>
2023-01-16 12:03:58 +0000

    16/01

--- app/src/main/java/com/example/gymapp/MainActivity.java
+++ app/src/main/java/com/example/gymapp/MainActivity.java
@@ -3,9 +3,12 @@ package com.example.gymapp;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.util.Log;
 import android.view.View;
 import android.widget.Button;
 import android.widget.EditText;
+import android.widget.LinearLayout;
+import android.widget.TextView;
 
 import com.android.volley.Request;
 import com.android.volley.RequestQueue;
@@ -15,6 +18,7 @@ import com.android.volley.toolbox.JsonObjectRequest;
 import com.android.volley.toolbox.StringRequest;
 import com.android.volley.toolbox.Volley;
 
+import org.json.JSONArray;
 import org.json.JSONException;
 import org.json.JSONObject;
 
@@ -26,9 +30,12 @@ public class MainActivity extends AppCompatActivity {
         setContentView(R.layout.activity_main);
     }
 
-    public void pesquisarUser(View view){
+    public void pesquisarUser(View view) throws JSONException{
         String email = ((EditText)findViewById(R.id.edEmail)).getText().toString();
         Button btnLogin = (Button)findViewById(R.id.btnLogin);
+
+        RequestQueue queue = Volley.newRequestQueue(getApplicationContext());
+        String url = "http://192.168.55.47/medion/loginandroid.php";
 
         JSONObject jsonObj = new JSONObject();
         try {
@@ -37,10 +44,32 @@ public class MainActivity extends AppCompatActivity {
             e.printStackTrace();
         }
 
-        System.out.println("hello");
+        JsonObjectRequest postRequest = new JsonObjectRequest (Request.Method.POST, url,jsonObj,
+                response ->
+                {
+                    try
+                    {
+                        JSONObject resposta = new JSONObject(response.toString());
+                        Log.d("MDLog", resposta.toString());
+                        JSONObject nomeUser = resposta.getJSONObject("edNome");
+                        LinearLayout llContainer = findViewById(R.id.container);
+                        llContainer.removeAllViews();
 
-        RequestQueue queue = Volley.newRequestQueue(getApplicationContext());
-        String url = "http://192.168.1.215:5000/search";
+                            TextView nTextView = new TextView(getApplicationContext());
+                            nTextView.setText(nomeUser.toString());
+                            llContainer.addView(nTextView);
+
+                    } catch (JSONException e)
+                    {
+                        e.printStackTrace();
+                    }
+                },
+                error -> {
+                    // error
+                    //Log.d("MDLog", error.getMessage());
+                }
+        );
+        queue.add(postRequest);
 
         btnLogin.setOnClickListener(new View.OnClickListener() {
             @Override
