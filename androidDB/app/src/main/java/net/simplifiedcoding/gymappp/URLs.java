cfc8bf0ea93e95426d1e7b33aea719fb2b9d6e22
joaojacinto <joaorsj96@gmail.com>
2023-01-19 09:53:55 +0000

    19-01

---
+++ androidDB/app/src/main/java/net/simplifiedcoding/gymappp/URLs.java
@@ -0,0 +1,10 @@
+package net.simplifiedcoding.gymappp;
+
+public class URLs {
+
+    private static final String ROOT_URL = "http://192.168.55.11/android_db_test/Api.php?apicall=";
+
+    public static final String URL_REGISTER = ROOT_URL + "signup";
+    public static final String URL_LOGIN = ROOT_URL + "login";
+
+}
