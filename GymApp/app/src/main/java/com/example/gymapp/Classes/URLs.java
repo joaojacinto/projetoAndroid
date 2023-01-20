74374b4411be70354a0efe6ad7bcc3f8ae6c8070
joaojacinto <joaorsj96@gmail.com>
2023-01-20 11:09:43 +0000

    localproperties

--- GymApp/app/src/main/java/com/example/gymapp/Classes/URLs.java
+++ GymApp/app/src/main/java/com/example/gymapp/Classes/URLs.java
@@ -2,7 +2,7 @@ package com.example.gymapp.Classes;
 
 public class URLs {
 
-    private static final String ROOT_URL = "http://192.168.1.213/android_db_test/Api.php?apicall=";
+    private static final String ROOT_URL = "http://192.168.55.14/android_db_test/Api.php?apicall=";
 
     public static final String URL_REGISTER = ROOT_URL + "signup";
 
